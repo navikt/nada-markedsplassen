f4e40de447e5f2cf543971f44f973907d922a633
Erik Vattekar <erik.vattekar@nav.no>
2024-09-12 12:38:06 +0200

    fix(metabase): remove migration adding bigquery datasource when dataset deleted constraint
    Co-authored-by: Paul B. Beskow <paul@beskow.no>

--- pkg/database/migrations/0095_set_fk_on_bigquery_datasource_dataset_id.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE datasource_bigquery ADD CONSTRAINT fk_bigquery_dataset
-    FOREIGN KEY (dataset_id)
-        REFERENCES datasets (id) ON DELETE CASCADE;
-
--- +goose Down
-ALTER TABLE datasource_bigquery DROP CONSTRAINT fk_bigquery_dataset;
