3a881d6db94c2803cf2b6220e88c5a5d3d534f13
meng.zhu@nav.no <meng.zhu@nav.no>
2024-10-16 15:12:51 +0200

    fix api

--- frontend/lib/rest/request.ts
+++ frontend/lib/rest/request.ts
@@ -1,33 +1,35 @@
 const makeError = async (res: Response) => {
     const body = await res.text()
     return {
-      message: body,
-      status: res.status,
-      id: res.headers.get('X-Correlation-Id'),
+        message: body,
+        status: res.status,
+        id: res.headers.get('X-Correlation-Id'),
     }
-  }
+}
 
 export const apiTemplate = (url: string, method: string, body?: any) => fetch(url, {
     method: method,
     credentials: 'include',
     headers: {
-      'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
     },
     body: JSON.stringify(body),
-  }).then(async res => {
+}).then(async res => {
     if (!res.ok) {
-      throw await makeError(res)
+        throw await makeError(res)
     }
-    //if response is 2xx, the response body should be json object
-    return res.json()
-  })
-  
-const curriedApiTemplate = (method: string) => (body?: string) => (url:string) => apiTemplate(url, method, body)
+
+    const contentType = res.headers.get('Content-Type')
+
+    return contentType && contentType.includes('application/json') ? res.json() : null
+})
+
+const curriedApiTemplate = (method: string) => (body?: string) => (url: string) => apiTemplate(url, method, body)
 
 export const fetchTemplate = curriedApiTemplate('GET')()
-  
+
 export const postTemplate = (url: string, body?: any) => curriedApiTemplate('POST')(body)(url)
 
 export const putTemplate = (url: string, body?: any) => curriedApiTemplate('PUT')(body)(url)
-  
+
 export const deleteTemplate = (url: string) => curriedApiTemplate('DELETE')()(url)
