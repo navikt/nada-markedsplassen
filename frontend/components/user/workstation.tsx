9c0f7306e3c4f62f70280ef28b9f7a12d89cefd2
Petter Jacobsen <petter.jacobsen@nav.no>
2024-10-16 13:50:05 +0200

    feat(workstations): Added input for urllist
    Co-authored-by: Erik Vattekar <erik.vattekar@nav.no>
    Co-authored-by: Paul B. Beskow <paul@beskow.no>

--- frontend/components/user/workstation.tsx
+++ frontend/components/user/workstation.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Heading, Select, UNSAFE_Combobox } from "@navikt/ds-react"
+import { Alert, Button, Heading, Select, UNSAFE_Combobox, Textarea, Label, Link } from "@navikt/ds-react"
 import {
     ensureWorkstation,
     startWorkstation,
@@ -16,6 +16,7 @@ import {
 } from "../../lib/rest/generatedDto";
 import TagsSelector from "../lib/tagsSelector";
 import TagPill from "../lib/tagPill";
+import { Header } from "@navikt/ds-react-internal";
 
 interface WorkstationStateProps {
     workstationData?: any
@@ -96,17 +97,17 @@ export const Workstation = () => {
     }
 
     const handleFirewallTagChange = (tagValue: string, isSelected: boolean) => {
-        if(isSelected) {
+        if (isSelected) {
             setSelectedFirewallTags(new Set(selectedFirewallTags.add(tagValue)))
             return
-        } 
+        }
         selectedFirewallTags.delete(tagValue)
 
         setSelectedFirewallTags(new Set(selectedFirewallTags))
     }
 
     return (
-        <div>
+        <div className="flex">
             <form
                 onSubmit={handleOnCreateOrUpdate}>
                 <div className="flex flex-col gap-8">
@@ -125,14 +126,20 @@ export const Workstation = () => {
                                 "Could not load container image"
                         ))}                    </Select>
                     <UNSAFE_Combobox
-                        label="Velg hvilke onprem-kilder du trenger åpninger mot."
-                        options={workstationOptions? workstationOptions.firewallTags?.map((o: FirewallTag | undefined) => (o? {
+                        label="Velg hvilke onprem-kilder du trenger åpninger mot"
+                        options={workstationOptions ? workstationOptions.firewallTags?.map((o: FirewallTag | undefined) => (o ? {
                             label: `${o?.name}`,
                             value: o?.secureTag,
-                          }: {label: "Could not load firewall tag", value: "Could not load firewall tag"})): []}
+                        } : { label: "Could not load firewall tag", value: "Could not load firewall tag" })) : []}
                         isMultiSelect
                         onToggleSelected={handleFirewallTagChange}
                     />
+                    <div className="flex gap-2 flex-col">
+                        <Label>Oppgi hvilke internett-URL-er du vil åpne mot</Label>
+                        <p className="pt-0">Du kan legge til opptil 2500 oppføringer i en URL-liste. Hver oppføring må stå på en egen linje uten mellomrom eller skilletegn. Oppføringer kan være kun domenenavn (som matcher alle stier) eller inkludere en sti-komponent. <Link href="https://cloud.google.com/secure-web-proxy/docs/url-list-syntax-reference">Les mer om syntax her.</Link></p>
+
+                        <Textarea size="medium" maxRows={2500} hideLabel label="Hvilke URL-er vil du åpne mot" resize />
+                    </div>
                     <div className="flex flex-row gap-3">
                         <Button variant="secondary" onClick={() => {
                         }}>
@@ -144,12 +151,14 @@ export const Workstation = () => {
                         }
                     </div>
                 </div>
+                <div className="flex flex-col">
+                    <Button onClick={() => {
+                    }}>Endre</Button>
+                    <WorkstationState workstationData={workstation} handleOnStart={handleOnStart}
+                        handleOnStop={handleOnStop} />
+                </div>
             </form>
-            <div className="flex flex-col">
-                <Button onClick={() => {
-                }}>Endre</Button>
-                <WorkstationState workstationData={workstation} handleOnStart={handleOnStart}
-                    handleOnStop={handleOnStop} />
+            <div className="flex flex-col gap-4">
             </div>
         </div>
     )
