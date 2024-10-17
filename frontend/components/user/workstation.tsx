813e5c3589310664a3f4f0ed7ac52794e6b2521d
Erik Vattekar <erik.vattekar@nav.no>
2024-10-17 12:13:42 +0200

    fix(workstations): add external link icon nad open in new tab for urllist syntax link

--- frontend/components/user/workstation.tsx
+++ frontend/components/user/workstation.tsx
@@ -19,6 +19,7 @@ import {
 import TagsSelector from "../lib/tagsSelector";
 import TagPill from "../lib/tagPill";
 import { Header } from "@navikt/ds-react-internal";
+import { ExternalLink } from "@navikt/ds-icons";
 
 interface WorkstationStateProps {
     workstationData?: any
@@ -208,7 +209,7 @@ export const Workstation = () => {
                         />
                         <div className="flex gap-2 flex-col">
                             <Label>Oppgi hvilke internett-URL-er du vil åpne mot</Label>
-                            <p className="pt-0">Du kan legge til opptil 2500 oppføringer i en URL-liste. Hver oppføring må stå på en egen linje uten mellomrom eller skilletegn. Oppføringer kan være kun domenenavn (som matcher alle stier) eller inkludere en sti-komponent. <Link href="https://cloud.google.com/secure-web-proxy/docs/url-list-syntax-reference">Les mer om syntax her.</Link></p>
+                            <p className="pt-0">Du kan legge til opptil 2500 oppføringer i en URL-liste. Hver oppføring må stå på en egen linje uten mellomrom eller skilletegn. Oppføringer kan være kun domenenavn (som matcher alle stier) eller inkludere en sti-komponent. <Link target="_blank" href="https://cloud.google.com/secure-web-proxy/docs/url-list-syntax-reference">Les mer om syntax her <ExternalLink /></Link></p>
 
                             <Textarea size="medium" maxRows={2500} hideLabel label="Hvilke URL-er vil du åpne mot" resize />
                         </div>
