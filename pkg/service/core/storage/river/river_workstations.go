d3db6a7e3ccbaed9881ca9623fb3ffa9981b0365
Paul B. Beskow <paul@beskow.no>
2024-10-22 13:18:29 +0200

    feat(workstations): create producer

---
+++ pkg/service/core/storage/river/river_workstations.go
@@ -0,0 +1,180 @@
+package river
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/navikt/nada-backend/pkg/database"
+	"github.com/navikt/nada-backend/pkg/database/gensql"
+	"github.com/navikt/nada-backend/pkg/errs"
+	"github.com/navikt/nada-backend/pkg/service"
+	"github.com/navikt/nada-backend/pkg/workers/workstations/args"
+	"github.com/riverqueue/river"
+	"github.com/riverqueue/river/riverdriver/riverdatabasesql"
+	"github.com/riverqueue/river/rivertype"
+	"time"
+)
+
+type workstationsStorage struct {
+	workers *river.Workers
+	repo    *database.Repo
+}
+
+func fromRiverJob(job *rivertype.JobRow) (*service.WorkstationJob, error) {
+	a := &args.WorkstationJob{}
+
+	err := json.NewDecoder(bytes.NewReader(job.EncodedArgs)).Decode(a)
+	if err != nil {
+		return nil, fmt.Errorf("decoding workstation job args: %w", err)
+	}
+
+	var state service.WorkstationJobState
+	var allErrs []string
+
+	switch job.State {
+	case rivertype.JobStateAvailable, rivertype.JobStateRunning, rivertype.JobStateRetryable:
+		state = service.WorkstationJobStateRunning
+	case rivertype.JobStateCompleted:
+		state = service.WorkstationJobStateCompleted
+	case rivertype.JobStateDiscarded:
+		state = service.WorkstationJobStateFailed
+		for _, e := range job.Errors {
+			allErrs = append(allErrs, e.Error)
+		}
+	}
+
+	return &service.WorkstationJob{
+		Name:            a.Name,
+		Email:           a.Email,
+		Ident:           a.Ident,
+		MachineType:     a.MachineType,
+		ContainerImage:  a.ContainerImage,
+		URLAllowList:    a.URLAllowList,
+		OnPremAllowList: a.OnPremAllowList,
+		StartTime:       job.CreatedAt,
+		State:           state,
+		Errors:          allErrs,
+	}, nil
+}
+
+func (s *workstationsStorage) GetWorkstationJob(ctx context.Context, ident string) (*service.WorkstationJob, error) {
+	const op errs.Op = "workstationsStorage.GetWorkstationJob"
+
+	raw, err := s.repo.Querier.GetWorkstationsJob(ctx, ident)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errs.E(errs.NotExist, op, err)
+		}
+
+		return nil, errs.E(errs.Database, op, err)
+	}
+
+	client, err := s.newClient()
+	if err != nil {
+		return nil, errs.E(errs.IO, op, err)
+	}
+
+	j, err := client.JobGet(ctx, raw.JobID)
+	if err != nil {
+		return nil, errs.E(errs.IO, op, err)
+	}
+
+	job, err := fromRiverJob(j)
+	if err != nil {
+		return nil, errs.E(errs.Internal, op, err)
+	}
+
+	return job, nil
+}
+
+func (s *workstationsStorage) CreateWorkstationOperation(ctx context.Context, opts *service.WorkstationJobOpts) (*service.WorkstationJob, error) {
+	const op errs.Op = "workstationsStorage.CreateWorkstationOperation"
+
+	client, err := s.newClient()
+	if err != nil {
+		return nil, errs.E(errs.IO, op, err)
+	}
+
+	tx, err := s.repo.GetDB().BeginTx(ctx, nil)
+	if err != nil {
+		return nil, errs.E(errs.Database, op, fmt.Errorf("starting workstations worker transaction: %w", err))
+	}
+
+	insertOpts := &river.InsertOpts{
+		MaxAttempts: 5,
+		Metadata:    nil,
+		Queue:       "",
+		Tags:        nil,
+		UniqueOpts: river.UniqueOpts{
+			ByArgs:   true,
+			ByPeriod: 1 * time.Hour,
+			ByState: []rivertype.JobState{
+				rivertype.JobStateAvailable,
+				rivertype.JobStatePending,
+				rivertype.JobStateRunning,
+				rivertype.JobStateRetryable,
+				rivertype.JobStateScheduled,
+			},
+		},
+	}
+
+	raw, err := client.InsertTx(ctx, tx, &args.WorkstationJob{
+		Ident:           opts.User.Ident,
+		Email:           opts.User.Name,
+		Name:            opts.User.Email,
+		MachineType:     opts.Input.MachineType,
+		ContainerImage:  opts.Input.ContainerImage,
+		URLAllowList:    opts.Input.URLAllowList,
+		OnPremAllowList: opts.Input.OnPremAllowList,
+	}, insertOpts)
+	if err != nil {
+		return nil, errs.E(errs.IO, op, err)
+	}
+
+	if !raw.UniqueSkippedAsDuplicate {
+		err = s.repo.Querier.WithTx(tx).CreateWorkstationsJob(ctx, gensql.CreateWorkstationsJobParams{
+			UserIdent: opts.User.Ident,
+			JobID:     raw.Job.ID,
+		})
+		if err != nil {
+			return nil, errs.E(errs.Database, op, err)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, errs.E(errs.Database, op, fmt.Errorf("committing workstations worker transaction: %w", err))
+	}
+
+	job, err := fromRiverJob(raw.Job)
+	if err != nil {
+		return nil, errs.E(errs.Internal, op, err)
+	}
+
+	return job, nil
+}
+
+func (s *workstationsStorage) newClient() (*river.Client[*sql.Tx], error) {
+	// Create an insert-only client
+	// - https://github.com/riverqueue/river?tab=readme-ov-file#insert-only-clients
+	client, err := river.NewClient(riverdatabasesql.New(s.repo.GetDB()),
+		&river.Config{
+			Workers: s.workers,
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("creating river client: %w", err)
+	}
+
+	return client, nil
+}
+
+func NewWorkstationsStorage(workers *river.Workers, repo *database.Repo) *workstationsStorage {
+	return &workstationsStorage{
+		workers: workers,
+		repo:    repo,
+	}
+}
