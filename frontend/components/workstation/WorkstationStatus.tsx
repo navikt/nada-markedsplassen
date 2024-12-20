4cfc3c4f812ad8357a56fe65aa4c442184295864
Petter Jacobsen <petter.jacobsen@nav.no>
2024-12-20 13:50:28 +0100

    feat(workstation): Improve modal description for remote ssh

--- frontend/components/workstation/WorkstationStatus.tsx
+++ frontend/components/workstation/WorkstationStatus.tsx
@@ -119,15 +119,14 @@ const WorkstationStatus = () => {
                                     <List.Item>
                                         Installere extension <code>Remote - SSH</code> i VS Code
                                     </List.Item>
-                                    <List.Item>
-                                        <p>Logg inn i Google Cloud (kjøres lokalt):</p> <div className="flex">
+                                    <List.Item title={"Logg inn i Google Cloud (kjøres lokalt)"}>
+                                        <div className="flex">
                                         <code
                                             className="rounded-sm bg-surface-neutral-subtle font-mono text-sm font-semibold">gcloud
                                             auth login</code><CopyButton size="xsmall" copyText="gcloud auth login"/>
                                         </div>
                                     </List.Item>
-                                    <List.Item>
-                                        <p>Opprette SSH-tunnel (kjøres lokalt):</p>
+                                    <List.Item title={"Opprette SSH-tunnel (kjøres lokalt)"}>
                                         <div className="flex">
                                         <code
                                             className="rounded-sm bg-surface-neutral-subtle px-1 py-05 font-mono text-sm font-semibold">
@@ -135,14 +134,13 @@ const WorkstationStatus = () => {
                                         <CopyButton size="xsmall" copyText={`gcloud workstations start-tcp-tunnel --cluster=knada --config=${workstation.data.slug} --region=europe-north1 --project knada-gcp --local-host-port=:33649 ${workstation.data.slug} 22`}/>
                                         </div>
                                     </List.Item>
-                                    <List.Item>
-                                        <p>Opprette ssh-nøkkelpar (kjøres lokalt, hopp over om du allerede har gjort dette):</p>
+                                    <List.Item title={"Opprette SSH-nøkkel (kjøres lokalt, hopp over om du allerede har gjort dette)"}>
+                                        <em>Sett et passord på SSH-nøkkelen. Du vil aldri bli bedt om å bytte dette.</em>
                                         <div className="flex">
                                             <code className="rounded-sm bg-surface-neutral-subtle font-mono text-sm font-semibold">{`ssh-keygen -t ed25519 -C "din_epost_email@nav.no"`}</code><CopyButton size="xsmall" copyText={`ssh-keygen -t ed25519 -C "din_epost_email@nav.no"`}></CopyButton>
                                         </div>
                                     </List.Item>
-                                    <List.Item>
-                                        <p>Få Knast til å stole på din SSH-nøkkel (kjøres på Knast, hopp over om du allerede har gjort dette):</p>
+                                    <List.Item title={"Få Knast til å stole på din SSH-nøkkel (kjøres på Knast, hopp over om du allerede har gjort dette)"}>
                                         <ul>
                                             <li>Opprette directory <strong>~/.ssh/</strong> hvis det ikke allerede finnes</li>
                                             <li>Opprett filen <strong>authorized_keys</strong> i <strong>~/.ssh/</strong></li>
@@ -150,14 +148,24 @@ const WorkstationStatus = () => {
                                         </ul>
 
                                     </List.Item>
-                                    <List.Item><p>Trykk cmnd+shift+P (mac) eller ctrl+shift+P (windows/linux) og skriv inn/velg:
-                                        <code>Remote - SSH: Connect to host...</code> og skriv inn:</p>
+                                    <List.Item title={"Legg til knast i ssh-configen (kjøres lokalt, hopp over om du allerede har gjort dette)"}>
                                         <div className="flex">
-                                            <code className="rounded-sm bg-surface-neutral-subtle font-mono text-sm font-semibold">user@localhost:33649</code><CopyButton size="xsmall" copyText={`user@localhost:33649`}></CopyButton>
+                                            <code
+                                                className="rounded-sm bg-surface-neutral-subtle font-mono text-sm font-semibold">{`echo -e "\\nHost knast\\n\\tHostName localhost\\n\\tPort 33649\\n\\tUser user">>~/.ssh/config`}</code><CopyButton
+                                            size="xsmall"
+                                            copyText={`echo -e "\\nHost knast\\n\\tHostName localhost\\n\\tPort 33649\\n\\tUser user">>~/.ssh/config`}></CopyButton>
                                         </div>
                                     </List.Item>
+                                    <List.Item title={"Åpne Command Palette i VS Code (⇧⌘P / Ctrl+Shift+P)"}>
+                                        <ul>
+                                            <li>Velg/Skriv inn: <code>Remote - SSH: Connect to host...</code></li>
+                                            <li>Skriv inn: <strong>knast</strong></li>
+                                        </ul>
+                                    </List.Item>
                                 </List>
-                                <p>Dette er også beskrevet med skjermbilder i <Link href="https://cloud.google.com/workstations/docs/develop-code-using-local-vscode-editor">dokumentasjonen til Google Workstations</Link></p>
+                                <p>Dette er også beskrevet med skjermbilder i <Link
+                                    href="https://cloud.google.com/workstations/docs/develop-code-using-local-vscode-editor">dokumentasjonen
+                                    til Google Workstations.</Link></p>
                             </BodyLong>
                         </Modal.Body>
                     </Modal>
