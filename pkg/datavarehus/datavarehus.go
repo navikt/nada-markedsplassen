32b9ffa0a08513add37f7304c3cfbf0d4b904c27
Paul B. Beskow <paul@beskow.no>
2025-01-21 10:29:16 +0100

    feat(datavarehus): add parsing of tns names
    Co-authored-by: Erik Vattekar <erik.vattekar@nav.no>

--- pkg/datavarehus/datavarehus.go
+++ pkg/datavarehus/datavarehus.go
@@ -37,6 +37,67 @@ type Body struct {
 	SignedJWT string `json:"signed_jwt"`
 }
 
+type TNSNames struct {
+	Items []TNSName `json:"items"`
+
+	HasMore bool `json:"hasMore"`
+
+	Limit  int `json:"limit"`
+	Offset int `json:"offset"`
+	Count  int `json:"count"`
+
+	Links []Link `json:"links"`
+}
+
+type TNSName struct {
+	TnsName     string `json:"tns_name"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Host        string `json:"host"`
+	Port        string `json:"port"`
+	ServiceName string `json:"service_name"`
+}
+
+type Link struct {
+	Rel  string `json:"rel"`
+	Href string `json:"href"`
+}
+
+func (c *Client) GetTNSNames(ctx context.Context) ([]TNSName, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/ords/dvh/dvh_dmo/dmo_ops_tnsnames.json", c.host), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		resBody, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, fmt.Errorf("received status code %d from datavarehus: unable to parse response err: %w", res.StatusCode, err)
+		}
+
+		return nil, fmt.Errorf("received status code %d from datavarehus: error %s", res.StatusCode, string(resBody))
+	}
+
+	defer res.Body.Close()
+
+	n := &TNSNames{}
+	err = json.NewDecoder(res.Body).Decode(n)
+	if err != nil {
+		return nil, err
+	}
+
+	if n.HasMore {
+		return nil, fmt.Errorf("more items available than returned, please implement pagination")
+	}
+
+	return n.Items, nil
+}
+
 func (c *Client) SendJWT(ctx context.Context, keyID, signedJWT string) error {
 	if c.token.Expired() {
 		token, err := c.getAccessToken(ctx)
@@ -57,7 +118,7 @@ func (c *Client) SendJWT(ctx context.Context, keyID, signedJWT string) error {
 		return err
 	}
 
-	r, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/ords/dvh/dvh_dmo/snart/kommer", c.host), bytes.NewReader(b))
+	r, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/ords/dvh/dvh_dmo/snart/kommer", c.host), bytes.NewReader(b))
 	if err != nil {
 		return err
 	}
@@ -76,6 +137,7 @@ func (c *Client) SendJWT(ctx context.Context, keyID, signedJWT string) error {
 		if err != nil {
 			return fmt.Errorf("received status code %d from datavarehus: unable to parse response err: %w", res.StatusCode, err)
 		}
+
 		return fmt.Errorf("received status code %d from datavarehus: error %s", res.StatusCode, string(resBody))
 	}
 
@@ -83,18 +145,18 @@ func (c *Client) SendJWT(ctx context.Context, keyID, signedJWT string) error {
 }
 
 func (c *Client) getAccessToken(ctx context.Context) (*Token, error) {
-	r, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/ords/dvh/oauth/token", c.host), nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/ords/dvh/oauth/token", c.host), nil)
 	if err != nil {
 		return nil, err
 	}
 
-	q := r.URL.Query()
+	q := req.URL.Query()
 	q.Add("grant_type", "client_credentials")
-	r.URL.RawQuery = q.Encode()
+	req.URL.RawQuery = q.Encode()
 
-	r.SetBasicAuth(c.clientID, c.clientSecret)
+	req.SetBasicAuth(c.clientID, c.clientSecret)
 
-	resp, err := c.httpClient.Do(r)
+	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -105,7 +167,9 @@ func (c *Client) getAccessToken(ctx context.Context) (*Token, error) {
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println(token)
 	token.expires = time.Now().Add(time.Duration(token.ExpiresIn) * time.Second)
+	fmt.Println(token)
 
 	return token, nil
 }
---
+++ pkg/datavarehus/datavarehus_test.go
@@ -0,0 +1,178 @@
+package datavarehus_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/jarcoal/httpmock"
+	"github.com/navikt/nada-backend/pkg/datavarehus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func mockServer(statusCode int, responseBody string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(statusCode)
+		w.Write([]byte(responseBody))
+	}))
+}
+
+func TestClient_GetTNSNames(t *testing.T) {
+	tests := []struct {
+		name         string
+		statusCode   int
+		responseBody string
+		expectErr    bool
+		expect       any
+	}{
+		{
+			name:       "Success",
+			statusCode: http.StatusOK,
+			responseBody: func() string {
+				content, err := os.ReadFile("testdata/dmo_ops_tnsnames.json")
+				require.NoError(t, err)
+
+				return string(content)
+			}(),
+			expectErr: false,
+			expect: []datavarehus.TNSName{
+				{
+					TnsName:     "DVH-P",
+					Name:        "DVH-produksjon",
+					Description: "Produksjonsdatabasen til Nav-datavarehus. Den benyttes til ETL/lagring og konsum av dataobjekter i fagsystemsonen.",
+					Host:        "DM08-SCAN.ADEO.NO",
+					Port:        "1521",
+					ServiceName: "DWH_HA",
+				},
+				{
+					TnsName:     "DVH-U",
+					Name:        "DVH-utvikling",
+					Description: "Kopi av produksjon (manuelt oppdatert) som benyttes til utvikling.",
+					Host:        "DMV34-SCAN.ADEO.NO",
+					Port:        "1521",
+					ServiceName: "CCDWHU1_HA",
+				},
+			},
+		},
+		{
+			name:         "Error",
+			statusCode:   http.StatusInternalServerError,
+			responseBody: `{"error": "internal server error"}`,
+			expectErr:    true,
+			expect:       `received status code 500 from datavarehus: error {"error": "internal server error"}`,
+		},
+		{
+			name:         "HasMore",
+			statusCode:   http.StatusOK,
+			responseBody: `{"items": [], "hasMore": true}`,
+			expectErr:    true,
+			expect:       "more items available than returned, please implement pagination",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server := mockServer(tt.statusCode, tt.responseBody)
+			defer server.Close()
+
+			client := datavarehus.New(server.URL, "clientID", "clientSecret")
+			ctx := context.Background()
+
+			got, err := client.GetTNSNames(ctx)
+			if tt.expectErr {
+				require.Error(t, err)
+				assert.Equal(t, tt.expect, err.Error())
+			} else {
+				require.NoError(t, err)
+				assert.NotNil(t, got)
+				assert.Equal(t, tt.expect, got)
+			}
+		})
+	}
+}
+
+func TestClient_SendJWT(t *testing.T) {
+	tests := []struct {
+		name            string
+		keyID           string
+		signedJWT       string
+		token           *datavarehus.Token
+		tokenStatusCode int
+		expect          any
+		expectErr       bool
+	}{
+		{
+			name:      "Success",
+			keyID:     "78638r89867983r",
+			signedJWT: "eye038r039083.293r473092809tr8903.39r857039270953",
+			token: &datavarehus.Token{
+				AccessToken: "valid_token",
+				TokenType:   "bearer",
+				ExpiresIn:   3600,
+			},
+			tokenStatusCode: http.StatusOK,
+			expect: &datavarehus.Body{
+				KeyID:     "78638r89867983r",
+				SignedJWT: "eye038r039083.293r473092809tr8903.39r857039270953",
+			},
+			expectErr: false,
+		},
+		{
+			name:      "Error",
+			keyID:     "78638r89867983r",
+			signedJWT: "eye038r039083.293r473092809tr8903.39r857039270953",
+			token: &datavarehus.Token{
+				AccessToken: "invalid_token",
+				TokenType:   "bearer",
+				ExpiresIn:   3600,
+			},
+			tokenStatusCode: http.StatusInternalServerError,
+			expect:          "received status code 500 from datavarehus: error something bad",
+			expectErr:       true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+
+			token, err := json.Marshal(tc.token)
+			require.NoError(t, err)
+
+			httpmock.RegisterResponder("POST", "http://example.com/ords/dvh/oauth/token",
+				httpmock.NewBytesResponder(tc.tokenStatusCode, token))
+
+			httpmock.RegisterResponder("POST", "http://example.com/ords/dvh/dvh_dmo/snart/kommer",
+				func(request *http.Request) (*http.Response, error) {
+					require.Equal(t, fmt.Sprintf("Bearer %s", tc.token.AccessToken), request.Header.Get("Authorization"))
+					require.Equal(t, "application/json", request.Header.Get("Content-Type"))
+
+					if tc.expectErr {
+						return httpmock.NewStringResponse(http.StatusInternalServerError, "something bad"), nil
+					} else {
+						b := &datavarehus.Body{}
+						err := json.NewDecoder(request.Body).Decode(b)
+						require.NoError(t, err)
+						require.Equal(t, tc.expect, b)
+						return httpmock.NewStringResponse(http.StatusNoContent, ""), nil
+					}
+				})
+
+			client := datavarehus.New("http://example.com", "clientID", "clientSecret")
+			ctx := context.Background()
+
+			err = client.SendJWT(ctx, tc.keyID, tc.signedJWT)
+			if tc.expectErr {
+				assert.Error(t, err)
+				assert.Equal(t, tc.expect, err.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
