9db53d3b97525dd7be28386d1c91ac2bab996455
meng.zhu@nav.no <meng.zhu@nav.no>
2025-02-07 09:18:16 +0100

    automatically redirect

--- frontend/pages/story/[id]/[[...page]].tsx
+++ frontend/pages/story/[id]/[[...page]].tsx
@@ -6,13 +6,24 @@ const StoryProxy = ()=>{
     const router = useRouter()
 
     useEffect(()=>{
-        if (typeof window !== 'undefined' && window.umami) {
-            window.umami.track('view-story', {id: router.query.id})
-          }        
-        if(router.query.id){
-            router.push(`/quarto/${router.query.id}`)
+        const handleRouteChange = (url: string) => {
+            if (url === '/') {
+                if (typeof window !== 'undefined' && window.umami) {
+                    window.umami.track('view-story', {id: router.query.id})
+                }
+                if(router.query.id){
+                    router.push(`/quarto/${router.query.id}`)
+                }
+            }
         }
-    })
+
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.query.id])
+    
     return <>
         <p>Omdirigere...</p>
     </>;
