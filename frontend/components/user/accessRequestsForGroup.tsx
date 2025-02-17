09f71675a33e8e1459334fa67e6f8d4a695a3a40
meng.zhu@nav.no <meng.zhu@nav.no>
2025-02-17 13:20:18 +0100

    bugfix: Tilgangssøknader til meg error

--- frontend/components/user/accessRequestsForGroup.tsx
+++ frontend/components/user/accessRequestsForGroup.tsx
@@ -7,9 +7,9 @@ interface pendingAccessRequestsForGroupProps {
 
 export const AccessRequestsForGroup = ({ accessRequests }: pendingAccessRequestsForGroupProps) => {
     //TODO: support approved and denied requests
-    const pendingRequest = accessRequests.filter((r) => r.status === 'pending')
+    const pendingRequest = accessRequests?.filter((r) => r.status === 'pending')
     return <><div>
-        {pendingRequest?.length > 0 ? (<div>
+        {!!pendingRequest?.length ? (<div>
             {pendingRequest.map((r: any, i: number) => (
                 <PendingAccessRequestBar accessRequest={r} key={i}></PendingAccessRequestBar>
             ))
