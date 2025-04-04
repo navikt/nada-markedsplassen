a5dc78463a5941ab4258e4f75b837be70e1c95d2
Paul B. Beskow <paul@beskow.no>
2025-04-04 13:04:41 +0200

    feat(knast): how to setup poetry

--- frontend/components/workstation/WorkstationPythonSetup.tsx
+++ frontend/components/workstation/WorkstationPythonSetup.tsx
@@ -17,6 +17,19 @@ const WorkstationPythonSetup= () => {
                 <BodyShort size="medium">Med denne kommandoen logger du inn på Google Cloud med din personlige bruker og oppdaterer Application Default Credentials (ADC).</BodyShort>
                 <Heading size="small">pypi-auth</Heading>
                 <BodyShort size="medium">Denne kommandoen vil konfigurere <strong>.netrc</strong> filen i ditt <strong>$HOME</strong> directory for pypi artifact registry ved å bruke en autentiseringsnøkkel som er hentet fra <code>gcloud auth login --update-adc</code>. Denne nøkkelen er kortlevd, 1 time, og du må oppdatere den etter denne perioden ved å kjøre <code>pypi-auth</code> på nytt.</BodyShort>
+
+                <Heading size="medium">Oppsett av Poetry</Heading>
+                <BodyShort size="medium">For å bruke Poetry med pypi-proxy må du oppdatere <strong>pyproject.toml</strong> filen din med følgende innhold:</BodyShort>
+                <code>
+                    [[tool.poetry.source]]
+                    <br />
+                    name = "private"
+                    <br />
+                    url = "https://europe-north1-python.pkg.dev/knada-gcp/pypiproxy/simple/"
+                    <br />
+                    priority = "supplemental"
+                </code>
+                <BodyShort size="medium">Etter dette kan du kjøre <code>poetry add ...</code> som vanlig.</BodyShort>
             </VStack>
         </div>
     );
