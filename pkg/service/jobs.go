e3c5a7e94a633b5f6b5d092cfef2a4b14813a415
Paul B. Beskow <paul@beskow.no>
2025-05-15 14:56:40 +0200

    chore(service): move the job stuff into own file

---
+++ pkg/service/jobs.go
@@ -0,0 +1,20 @@
+package service
+
+import "time"
+
+type JobHeader struct {
+	ID        int64      `json:"id"`
+	StartTime time.Time  `json:"startTime"`
+	EndTime   *time.Time `json:"endTime"`
+	State     JobState   `json:"state"`
+	Duplicate bool       `json:"duplicate"`
+	Errors    []string   `json:"errors"`
+}
+
+type JobState string
+
+const (
+	JobStateCompleted JobState = "COMPLETED"
+	JobStateRunning   JobState = "RUNNING"
+	JobStateFailed    JobState = "FAILED"
+)
