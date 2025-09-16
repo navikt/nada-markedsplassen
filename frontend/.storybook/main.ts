0043538028f2b12e06e55e4e3b762d6c00cfd5f1
vebjorre <vebjorn.rekkebo@nav.no>
2025-09-16 14:48:51 +0200

    chore(deps): migrate storybook to v9

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -7,9 +7,8 @@ const config: StorybookConfig = {
   ],
   addons: [
     '@storybook/addon-onboarding',
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
+    '@storybook/addon-docs'
   ],
   framework: {
     name: '@storybook/nextjs',
