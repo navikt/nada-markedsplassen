8c9a1225543fae3efbc1d3ccc524cd6892dd80d8
meng.zhu@nav.no <meng.zhu@nav.no>
2025-11-05 09:44:55 +0100

    remove hoved effect

--- frontend/components/knast/widgets/iconButton.tsx
+++ frontend/components/knast/widgets/iconButton.tsx
@@ -11,12 +11,8 @@ interface IconButtonProps {
 }
 
 export const IconButton = ({ x, y, className, tooltip, ariaLabel, onClick, children }: IconButtonProps) => {
-    const [hover, setHover] = React.useState(false)
-    const childWithHover = React.cloneElement(children as React.ReactElement, { state: hover ? "hover" : "normal"})
     return <div className={`absolute -translate-x-1/2 -translate-y-1/2 ${className}`} style={{ left: x, top: y }} onClick={onClick} aria-label={ariaLabel} title={tooltip}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
     >
-        {childWithHover}
+        {children}
     </div>
 }
