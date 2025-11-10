2eafb360021b9b70a67687f115cd8bb1d93b0e5f
meng.zhu@nav.no <meng.zhu@nav.no>
2025-11-10 14:27:16 +0100

    fix knast url

--- frontend/components/knast/widgets/openKnastLink.tsx
+++ frontend/components/knast/widgets/openKnastLink.tsx
@@ -1,20 +1,20 @@
 import { Link, Popover, Tooltip } from "@navikt/ds-react"
-import { buildUrl } from "../../../lib/rest/apiUrl"
+import { buildUrl, buildUrlAsClientWithoutProxy } from "../../../lib/rest/apiUrl"
 import { ExternalLinkIcon, InformationSquareFillIcon } from "@navikt/aksel-icons"
 import { NaisdeviceGreen } from "../../lib/icons/naisdeviceGreen"
 import React from "react"
 
 const NaisdevicePopoverContent = () => {
-  return (
-    <Popover.Content>
-      <div className="flex flex-row items-end">
+    return (
+        <Popover.Content>
+            <div className="flex flex-row items-end">
                 <NaisdeviceGreen />
 
-        Er naisdevice &nbsp;
-        <p className="text-[#78c010]">grønn?</p>
-      </div>
-    </Popover.Content>
-  )
+                Er naisdevice &nbsp;
+                <p className="text-[#78c010]">grønn?</p>
+            </div>
+        </Popover.Content>
+    )
 }
 
 export const OpenKnastLink = ({ knastInfo, caption: caption }: { knastInfo: any, caption?: string }) => {
@@ -22,21 +22,21 @@ export const OpenKnastLink = ({ knastInfo, caption: caption }: { knastInfo: any,
     const linkRef = React.useRef<HTMLAnchorElement | null>(null);
     const handleOpenKnastInBrowser = () => {
         if (knastInfo?.host) {
-            window.open(buildUrl('googleOauth2')('login')({ redirect: `https://${knastInfo.host}/` }), '_blank')
+            window.open(buildUrlAsClientWithoutProxy('googleOauth2')('login')({ redirect: `https://${knastInfo.host}/` }), '_blank')
         }
     }
 
     return <div>
-            <Popover
+        <Popover
             className="w-60"
-              open={showNaisdeviceInfo}
-              onClose={() => setShowNaisdeviceInfo(false)}
-                anchorEl={linkRef.current}
-            >
-              <NaisdevicePopoverContent />
-            </Popover>        
-        <Link onMouseEnter={()=>setShowNaisdeviceInfo(true)} 
-            onMouseLeave={()=>setShowNaisdeviceInfo(false)}
-        onClick={handleOpenKnastInBrowser} ref={linkRef}> {caption ?? `Åpne ${knastInfo?.imageTitle}`}<ExternalLinkIcon /></Link>
+            open={showNaisdeviceInfo}
+            onClose={() => setShowNaisdeviceInfo(false)}
+            anchorEl={linkRef.current}
+        >
+            <NaisdevicePopoverContent />
+        </Popover>
+        <Link onMouseEnter={() => setShowNaisdeviceInfo(true)}
+            onMouseLeave={() => setShowNaisdeviceInfo(false)}
+            onClick={handleOpenKnastInBrowser} ref={linkRef}> {caption ?? `Åpne ${knastInfo?.imageTitle}`}<ExternalLinkIcon /></Link>
     </div>
 }
