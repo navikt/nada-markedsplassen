51b8f9f5f3fb76ad375327b8396f325df113d3b3
Petter Jacobsen <petter.jacobsen@nav.no>
2026-05-11 13:00:57 +0200

    Added new test

--- pkg/worker/workstation_test.go
+++ pkg/worker/workstation_test.go
@@ -14,8 +14,9 @@ import (
 
 type workstationURLListServiceMock struct {
 	service.WorkstationsService
-	getActiveURLList func(ctx context.Context, user *service.User) (*service.WorkstationActiveURLListForIdent, error)
-	ensureURLList    func(ctx context.Context, input *service.WorkstationActiveURLListForIdent) error
+	getActiveURLList              func(ctx context.Context, user *service.User) (*service.WorkstationActiveURLListForIdent, error)
+	ensureURLList                 func(ctx context.Context, input *service.WorkstationActiveURLListForIdent) error
+	createEnsureURLListJobsForAll func(ctx context.Context) error
 }
 
 func (m *workstationURLListServiceMock) GetWorkstationActiveURLListForIdent(ctx context.Context, user *service.User) (*service.WorkstationActiveURLListForIdent, error) {
@@ -24,6 +25,10 @@ func (m *workstationURLListServiceMock) GetWorkstationActiveURLListForIdent(ctx 
 
 func (m *workstationURLListServiceMock) EnsureWorkstationURLList(ctx context.Context, input *service.WorkstationActiveURLListForIdent) error {
 	return m.ensureURLList(ctx, input)
+}
+
+func (m *workstationURLListServiceMock) CreateEnsureURLListJobsForAllIdents(ctx context.Context) error {
+	return m.createEnsureURLListJobsForAll(ctx)
 }
 
 func TestWorkstationEnsureURLListForIdent_Work_HappyPath(t *testing.T) {
@@ -93,4 +98,78 @@ func TestWorkstationEnsureURLListForIdent_Work_GetURLListError(t *testing.T) {
 	require.Error(t, err)
 	assert.ErrorContains(t, err, ident)
 	assert.ErrorContains(t, err, "getting workstation active urllist")
+}
+
+func TestWorkstationEnsureURLListForIdent_Work_EnsureURLListError(t *testing.T) {
+	ctx := context.Background()
+	ident := "nav-ident-3"
+	expectedErr := errors.New("gcp write failed")
+
+	mock := &workstationURLListServiceMock{
+		getActiveURLList: func(ctx context.Context, user *service.User) (*service.WorkstationActiveURLListForIdent, error) {
+			return &service.WorkstationActiveURLListForIdent{
+				Slug:    ident,
+				URLList: []string{"https://example.com"},
+			}, nil
+		},
+		ensureURLList: func(ctx context.Context, input *service.WorkstationActiveURLListForIdent) error {
+			return expectedErr
+		},
+	}
+
+	w := &WorkstationEnsureURLListForIdent{
+		service: mock,
+	}
+
+	job := &river.Job[worker_args.WorkstationEnsureURLListForIdent]{
+		Args: worker_args.WorkstationEnsureURLListForIdent{Ident: ident},
+	}
+
+	err := w.Work(ctx, job)
+	require.Error(t, err)
+	assert.ErrorContains(t, err, ident)
+	assert.ErrorContains(t, err, "ensuring workstation urllist")
+}
+
+func TestWorkstationEnsureURLList_Work_HappyPath(t *testing.T) {
+	ctx := context.Background()
+
+	called := false
+	mock := &workstationURLListServiceMock{
+		createEnsureURLListJobsForAll: func(ctx context.Context) error {
+			called = true
+			return nil
+		},
+	}
+
+	w := &WorkstationEnsureURLList{
+		service: mock,
+	}
+
+	job := &river.Job[worker_args.WorkstationEnsureURLList]{}
+
+	err := w.Work(ctx, job)
+	require.NoError(t, err)
+	assert.True(t, called, "CreateEnsureURLListJobsForAllIdents should have been called")
+}
+
+func TestWorkstationEnsureURLList_Work_CreateJobsError(t *testing.T) {
+	ctx := context.Background()
+	expectedErr := errors.New("db connection lost")
+
+	mock := &workstationURLListServiceMock{
+		createEnsureURLListJobsForAll: func(ctx context.Context) error {
+			return expectedErr
+		},
+	}
+
+	w := &WorkstationEnsureURLList{
+		service: mock,
+	}
+
+	job := &river.Job[worker_args.WorkstationEnsureURLList]{}
+
+	err := w.Work(ctx, job)
+	require.Error(t, err)
+	assert.ErrorContains(t, err, "creating ensure url list jobs")
 }
